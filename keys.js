817f4c5477b13e87636e8b6ba15fa6350e9d0ffc
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-10-04 15:15:38 -0500

    progress

--- keys.js
+++ keys.js
@@ -1,10 +1,14 @@
 console.log('this is loaded');
 
-var twitterKeys = new twitter({
+var twitterKeys = {
   consumer_key: 'EeNeL9ZSsq8rtfFA3NIb1177w',
   consumer_secret: 'bHch3In4AhI3VNwRC9NQ5OYe6AD4JO2xTTjvzgl6yeBJVwPbuS',
   access_token_key: '915284599365935104-KTEAWwXTTlY3Sam4lC1pEWLAdvMBof6',
   access_token_secret: 'tkfeqEueo06w7zHFPv1BjXUGIREAkj5NaAv0po1gG9cGC',
-});
+}
 
 module.exports = twitterKeys;
+
+// Spotify
+// Client ID: 576482252c1b476a8af4b3371a5bec27
+// Client Secret: c6f8afe0dbfa4cdd8b6feaf362e7c116
--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 var request = require('request');
+var fs = require('fs');
 var twitter = require ('twitter');
 var twitterKeys = require('./keys.js');
 
-console.log(twitterKeys);
+var client = new twitter(twitterKeys);
 
 /* Must take in the following arguments:
 `my-tweets`
@@ -12,28 +13,54 @@ console.log(twitterKeys);
 
 var command = process.argv[2];
 
+var variable = process.argv[3];
+
 switch (command) {
+
   case "my-tweets":
-  	//Unsure if this is correct
     var params = {screen_name: 'JfLiri'};
-	twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 	  if (!error) {
-	    console.log(tweets);
+	    for (var i = 0; i < tweets.length; i++) {
+	    	console.log(tweets[i].text);
+	    	console.log(tweets[i].created_at);
+	    };
 	  };
 	});
     break;
 
   case "spotify-this-song":
-    spotifySong();
     break;
 
   case "movie-this":
-    movieThis();
+  	var movie = "";
+  	var nodeArgs = process.argv;
+  	for (var i = 3; i < nodeArgs.length; i++) {
+  		if (i > 3 && i < nodeArgs.length) {
+  			movie = movie + "+" + nodeArgs[i];
+  		} else {
+  			movie += nodeArgs[i]
+  		}
+  	};
+  	var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+    request(queryURL, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			console.log(response);
+			console.log("Title: " + JSON.parse(body).Title);
+			console.log("Year: " + JSON.parse(body).Year);
+			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+			console.log("Country: " + JSON.parse(body).Country);
+			console.log("Language: " + JSON.parse(body).Language);
+			console.log("Plot: " + JSON.parse(body).Plot);
+			console.log("Actors: " + JSON.parse(body).Actors);
+		};
+	});		
     break;
 
   case "do-what-it-says":
-    doSays();
     break;
+
 };
 
 // request()
